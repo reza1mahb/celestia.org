fbc11c11145de7ee8849a6f91e5a95bdbcf608a0
Tamás <t.gabor80312@gmail.com>
2023-02-15 18:03:37 +0100

    hero section wip

--- src/pages/what-is-celestia.js
+++ src/pages/what-is-celestia.js
@@ -13,12 +13,20 @@ const WhatIsCelestia = () => {
 				<main>
 					<div className={"container"}>
 						<h1 className={"mb-3 mb-md-5"}>What is Celestia</h1>
-						<div className={"text mb-3 mb-md-5"}>
+						<p className={"lead-text"}>
 							A complete beginner's guide to how Celestia works, its key benefits and how anyone in the world will be able to create
 							their own blockchain in minutes.
-						</div>
-						<div className={"row card"}>
-							<div className={"col-6"}>text</div>
+						</p>
+						<div className={"card row justify-content-start"}>
+							<div className={"text-box col col-md-6"}>
+								<p>
+									Celestia is a new technology that powers, scales and secures Web3 applications. To achieve this, Celestia
+									introduces a new modular blockchain architecture that solves the core scaling problems of today’s blockchains.
+								</p>
+								<div className='title'>Confused?</div>
+								<p>Let’s walk through the basics, step-by-step.</p>
+							</div>
+							<div className="imagebox"></div>
 						</div>
 						<section className={"column-section"}>
 							<h4 className={"main"}>Section Title</h4>
