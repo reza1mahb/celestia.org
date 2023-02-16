e6ebc00796a867f398091b0a5c22c6271f410d6d
Tamás <t.gabor80312@gmail.com>
2023-02-16 17:55:37 +0100

    fixes

--- src/pages/what-is-celestia.js
+++ src/pages/what-is-celestia.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import { FooterBoxes } from "../datas/technology/content";
 import { Helmet } from "react-helmet";
-import { StaticImage } from "gatsby-plugin-image";
+// import { StaticImage } from "gatsby-plugin-image";
 
 const WhatIsCelestia = () => {
 	return (
@@ -19,26 +19,49 @@ const WhatIsCelestia = () => {
 							their own blockchain in minutes.
 						</p>
 
-						<section className={"card-style row justify-content-between"}>
-							<div className={"order-2 order-md-1 text-box col-12 col-md-6"}>
-								<p>
-									Celestia is a new technology that powers, scales and secures Web3 applications. To achieve this, Celestia
-									introduces a new modular blockchain architecture that solves the core scaling problems of today’s blockchains.
-								</p>
-								<div className='subtitle'>Confused?</div>
-								<p>Let’s walk through the basics, step-by-step.</p>
+						<section className={"card-section"}>
+							<div className={"row justify-content-between"}>
+								<div className={"order-2 order-lg-1 text-box col-12 col-lg-6"}>
+									<p>
+										Celestia is a new technology that powers, scales and secures Web3 applications. To achieve this, Celestia
+										introduces a new modular blockchain architecture that solves the core scaling problems of today’s blockchains.
+									</p>
+									<div className='subtitle'>Confused?</div>
+									<p>Let’s walk through the basics, step-by-step.</p>
+								</div>
+								<div className={"image-box--cover order-1 order-lg-2 col-12 col-lg-6"}></div>
 							</div>
-							<div className={"image-box image-box--cover order-1 order-md-2 col-12 col-md-6"}></div>
 						</section>
 
-						<section className={"two-column-box justify-content-between"}>
-							<div className={"row"}>
-								<div className={"text-box col-12 col-md-6"}>
+						<section className={"modular-blockchain-section"}>
+							<div className={"row justify-content-between"}>
+								<div className={"text-box col-12 col-lg-6"}>
 									<div className='title'>But what’s a modular blockchain?</div>
-									<p>Modular blockchains are a new paradigm in blockchain design. Instead of one blockchain doing everything, modular blockchains specialize and optimize to perform a given function. This specialization provides breakthroughs in scalability, flexibility, and interoperability, enabling developers to build blockchain applications for mass adoption.</p>
+									<p>
+										Modular blockchains are a new paradigm in blockchain design. Instead of one blockchain doing everything,
+										modular blockchains specialize and optimize to perform a given function. This specialization provides
+										breakthroughs in scalability, flexibility, and interoperability, enabling developers to build blockchain
+										applications for mass adoption.
+									</p>
 								</div>
-								<div className={"image-box--modular col-12 col-md-6"}>
-									<StaticImage src="../images/" />
+								<div className={"image-box--modular col-12 col-lg-6"} />
+							</div>
+						</section>
+
+						<section className={"how-different-section"}>
+							<div className={"row justify-content-between align-items-center"}>
+								<div className={"order-2 order-lg-1 image-box--different col-12 col-lg-6"} />
+								<div className={"order-1 order-lg-2 text-box col-12 col-lg-6"}>
+									<div className='title'>How is Celestia different from Bitcoin and Ethereum?</div>
+									<p>
+										<strong>If Bitcoin is a calculator and Ethereum is a computer, then Celestia is a cloud computer.</strong>
+									</p>
+									<p>
+										Bitcoin showed that it was possible to build a decentralized application but was very limited in what it could
+										be used to build. Ethereum showed that it was possible to build a programmable blockchain that anyone could
+										use to launch a decentralized application. Celestia will show that it is possible to build a network that
+										could enable anyone to launch their own blockchain.
+									</p>
 								</div>
 							</div>
 						</section>
