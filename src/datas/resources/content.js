68b8378c417f388af415d8fce3f837fa9bd550bc
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-02-27 23:34:43 -0600

    Update content.js

--- src/datas/resources/content.js
+++ src/datas/resources/content.js
@@ -1,21 +1,20 @@
 export const FooterBoxes = [
     {
-        title: 'Experiment with testnet',
-        text: 'Join a growing ecosystem of developers building rollups and applications on the Mocha testnet.',
+        title: 'Check out the forum',
+        text: 'Head to the forum to ask questions or post content.',
+        button: {
+            text: 'View now',
+            href: 'https://forum.celestia.org/',
+            id: 'career',
+            type: 'simple'
+        }
+    },{
+        title: 'Build on testnet',
+        text: 'Join a growing modular ecosystem of developers building on testnet.',
         button: {
             text: 'Get started',
             href: 'https://docs.celestia.org/',
             id: 'operator',
-            type: 'simple'
-        }
-    },
-    {
-        title: 'Join our growing team',
-        text: 'Join our team of leading engineers in pioneering the first modular blockchain network',
-        button: {
-            text: 'Current openings',
-            href: '/careers',
-            id: 'career',
             type: 'simple'
         }
     }
