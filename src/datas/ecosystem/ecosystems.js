36147bdfdd2d9c2a73178a5c3a57ae6040244b41
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-03-10 04:51:31 -0600

    Update ecosystems.js

--- src/datas/ecosystem/ecosystems.js
+++ src/datas/ecosystem/ecosystems.js
@@ -1,79 +1,160 @@
 export const ecosystemCategories = [
     {
         id : 1,
-        name : 'Cross chain',
+        name : 'Rollups as a service',
         ecosystems: [
             {
-                title: 'CNS',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/cns.png',
-                url: '/'
+                title: 'Caldera',
+                text: '',
+                image: '',
+                url: 'https://www.caldera.xyz/'
+            },
+            {
+                title: 'Dymension',
+                text: '',
+                image: '',
+                url: 'https://dymension.xyz/'
+            },
+            {
+                title: 'Eclipse',
+                text: '',
+                image: '',
+                url: 'https://www.eclipse.builders/'
             }
         ]
     },{
         id : 2,
-        name : 'MEV',
+        name : 'Sequencer as a service',
         ecosystems: [
             {
-                title: 'Skip',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/skip.png',
-                url: '/'
+                title: 'Saga',
+                text: '',
+                image: '',
+                url: 'https://www.saga.xyz/'
             }
         ]
     },{
         id : 3,
-        name : 'Rollums as a service',
-    },{
-        id : 4,
-        name : 'Optimistic rollup',
-    },{
-        id : 5,
-        name : 'ZK Rollup',
+        name : 'Rollup frameworks',
         ecosystems: [
             {
-                title: 'Sovereing',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/sovereign.png',
-                url: '/',
+                title: 'Rollkit',
+                text: '',
+                image: '',
+                url: 'https://www.rollkit.dev/'
+            },
+            {
+                title: 'Sovereign',
+                text: '',
+                image: '',
+                url: 'https://sovereign.xyz/'
+            }
+        ]
+    },{
+        id : 4,
+        name : 'Cross chain',
+        ecosystems: [
+            {
+                title: 'Axelar',
+                text: '',
+                image: 'ecosystem/axelar.png',
+                url: 'https://axelar.network/'
+            },
+            {
+                title: 'Polymer',
+                text: '',
+                image: 'ecosystem/polymer.png',
+                url: 'https://www.polymerlabs.org/'
+            },
+            {
+                title: 'Squid',
+                text: '',
+                image: 'ecosystem/squid.png',
+                url: 'https://www.squidrouter.com/'
+            }
+        ]
+    },{
+        id : 5,
+        name : 'Wallet',
+        ecosystems: [
+            {
+                title: 'Cosmostation',
+                text: '',
+                image: '',
+                url: 'https://www.cosmostation.io/'
+            },
+            {
+                title: 'Keplr',
+                text: '',
+                image: '',
+                url: 'https://www.keplr.app/'
+            },
+            {
+                title: 'Leap wallet',
+                text: '',
+                image: '',
+                url: 'https://www.leapwallet.io/'
             }
         ]
     },{
         id : 6,
-        name : 'Infrastructure',
-    },{
-        id : 7,
-        name : 'Wallet',
-    },{
-        id : 8,
-        name : 'Block Explorer',
-    },{
-        id : 9,
-        name : 'DEFI',
-    },{
-        id : 10,
-        name : 'Gaming',
-    },{
-        id : 11,
-        name : 'Bridge',
+        name : 'DeFi',
         ecosystems: [
             {
-                title: 'Polymer',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/polymer.png',
-                url: '/'
+                title: 'Duality',
+                text: '',
+                image: '',
+                url: 'https://duality.xyz/',
+            }
+        ]
+    },{
+        id : 7,
+        name : 'Gaming',
+        ecosystems: [
+            {
+                title: 'Argus',
+                text: '',
+                image: '',
+                url: 'https://argus.gg/'
+            }
+        ]
+    },{
+        id : 8,
+        name : 'Infrastructure',
+        ecosystems: [
+            {
+                title: 'PK Lab',
+                text: '',
+                image: '',
+                url: 'https://bakingbad.dev/'
             },
             {
-                title: 'Squid',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/squid.png',
-                url: '/'
-            },
+                title: 'Range',
+                text: '',
+                image: '',
+                url: 'https://www.range.org/'
+            }
+        ]
+    },{
+        id : 9,
+        name : 'MEV',
+        ecosystems: [
             {
-                title: 'Axelar',
-                text: '(in general use) a complex network or interconnected system. add 3 line description max.',
-                image: 'ecosystem/axelar.png',
-                url: '/'
+                title: 'Skip',
+                text: '',
+                image: 'ecosystem/skip.png',
+                url: 'https://skip.money/'
+            }
+        ]
+    },{
+        id : 10,
+        name : 'Block explorer',
+        ecosystems: [
+            {
+                title: 'Modular cloud',
+                text: '',
+                image: '',
+                url: 'https://explorer.modular.cloud/'
             }
         ]
     }
