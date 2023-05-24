f8929a10200361e0a09af8b9daaadf23bf92c75a
Tamás <t.gabor80312@gmail.com>
2023-05-24 11:10:58 +0200

    comp refactor

--- src/components/modules/advantage.js
+++ src/components/modules/advantage.js
@@ -1,34 +1,20 @@
-import * as React from "react";
+import React from "react";
 import Image from "../imageComponent";
-import handleViewport from "react-in-viewport";
 
-class Advantage extends React.Component {
-	getInViewPort() {
-		const { inViewport, enterCount, leaveCount } = this.props;
-		let counter = enterCount - leaveCount;
-		if (inViewport && counter === 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	render() {
-		return (
-			<div key={this.props.id} className={"col col-12 col-lg-3 gx-2 gy-2"}>
-				<div className={"advantage col"}>
-					<div className={"col image-wrapper"}>
-						<Image alt={this.props.title} filename={this.props.image} />
-					</div>
-					<div className={"col col-12"}>
-						<div className={"title"}>{this.props.title}</div>
-						<div className={"text"}>{this.props.text}</div>
-					</div>
+const Advantage = ({ key, title, text, image }) => {
+	return (
+		<div key={key} className={"col col-12 col-lg-3 gx-2 gy-2"}>
+			<div className={"advantage col"}>
+				<div className={"col image-wrapper"}>
+					<Image alt={title} filename={image} />
+				</div>
+				<div className={"col col-12"}>
+					<div className={"title"}>{title}</div>
+					<div className={"text"}>{text}</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
-const AdvantageBlock = handleViewport(Advantage, { rootMargin: "-1.0px" });
-
-export default AdvantageBlock;
+export default Advantage;
