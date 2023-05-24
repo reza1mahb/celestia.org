9a5b40ffc2a3506c6b98f56e1826a2c9e1eff691
Tamás <t.gabor80312@gmail.com>
2023-05-24 13:07:57 +0200

    build fix

--- src/components/sections/advantages-section.js
+++ src/components/sections/advantages-section.js
@@ -1,1 +1,17 @@
-w
+import React from "react";
+import Advantage from "../modules/advantage";
+
+const AdvantagesSection = ({ advantages }) => {
+	return (
+		<section className='advantages-container'>
+			<div className='row'>
+				{advantages &&
+					advantages.map(({ id, animation, text, title, image }) => (
+						<Advantage key={id} id={id} animation={animation} text={text} title={title} image={image} />
+					))}
+			</div>
+		</section>
+	);
+};
+
+export default AdvantagesSection;
