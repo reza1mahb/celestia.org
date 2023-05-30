1096d1732374ec58399badb6e99d7e2a7dbd794c
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-05-31 06:36:36 +0930

    Update advantages.js

--- src/datas/home/advantages.js
+++ src/datas/home/advantages.js
@@ -2,12 +2,12 @@ export const advantages = [
     {
         id: '1',
         title: 'Build',
-        text: 'Deploy a blockchain in minutes, as easily as a smart contract',
+        text: 'Deploy your own blockchain in minutes, as easily as a smart contract',
         image: 'advantages/advantage-1.png',
     },{
         id: '2',
         title: 'Scale',
-        text: 'Access the dynamic scaling unlocked by data availability sampling, where throughput increases with the number of users',
+        text: 'Access the dynamic scaling unlocked by <a href="https://celestia.org/glossary/data-availability-sampling/">data availability sampling</a>, where scale increases with the number of users',
         image: 'advantages/advantage-2.png',
     },{
         id: '3',
@@ -17,7 +17,7 @@ export const advantages = [
     },{
         id: '4',
         title: 'Sovereign',
-        text: 'Build sovereign rollups, a new type of <a href="https://www.google.com/">https://www.google.com/</a> self-governing blockchain with minimal platform risk',
+        text: 'Build <a href="https://celestia.org/glossary/sovereign-rollup/">sovereign rollups</a>, a new type of self-governing blockchain with minimal platform risk',
         image: 'advantages/advantage-4.png',
     },
 ]
