324113106da8cddc25558a2f5f42cfd0b91ceb28
Tamás <t.gabor80312@gmail.com>
2023-06-06 18:16:51 +0200

    adding community files

---
+++ src/datas/community/ecosystem.js
@@ -0,0 +1,11 @@
+export const ecosystem = {
+	title: "Ecosystem",
+	text: "Explore a wide range of apps and services built on Celestia",
+	image: "community/ecosystem.png",
+	button: {
+		text: "Show more",
+		class: "simple",
+		type: "internal",
+		url: "/",
+	},
+};
