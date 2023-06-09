119791cc8d57342b135980d659db0d103d8065c6
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-06-09 15:31:24 +0930

    Update hero-data.js

--- src/datas/community/hero-data.js
+++ src/datas/community/hero-data.js
@@ -1,18 +1,4 @@
 export const heroData = {
 	title: "Celestia Community",
-	text: "Build games with new capabilities. Celestia scales with the number of users so games can scale with their usebase.",
-	buttons: [
-		{
-			text: "Build Modular",
-			class: "simple",
-			type: "internal",
-			url: "/",
-		},
-		{
-			text: "Ask a question",
-			class: "white",
-			type: "internal",
-			url: "/",
-		},
-	],
+	text: "Discover community hubs, discussion forums, and resources that are used by the global Celestia community.",
 };
