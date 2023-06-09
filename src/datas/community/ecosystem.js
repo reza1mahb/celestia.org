cf809799f412a34d5ec87befe0b793ad59b8ccf7
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-06-09 15:20:33 +0930

    Update ecosystem.js

--- src/datas/community/ecosystem.js
+++ src/datas/community/ecosystem.js
@@ -1,11 +1,11 @@
 export const ecosystem = {
 	title: "Ecosystem",
-	text: "Explore a wide range of apps and services built on Celestia",
+	text: "Discover a wide range of apps and services built in the Celestia ecosystem.",
 	image: "community/ecosystem.png",
 	button: {
-		text: "Show more",
+		text: "Dive in",
 		class: "simple",
 		type: "internal",
-		url: "/",
+		url: "/ecosystem/",
 	},
 };
