7ca073cc82300d802f5b0a11911521a013f64fb7
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-06-09 15:34:38 +0930

    Update hero-data.js

--- src/datas/community/hero-data.js
+++ src/datas/community/hero-data.js
@@ -1,4 +1,18 @@
 export const heroData = {
 	title: "Celestia Community",
 	text: "Discover community hubs, discussion forums, and resources that are used by the global Celestia community.",
+	buttons: [
+    {
+        text: "",
+        class: "",
+        type: "",
+        url: "/",
+    },
+    {
+        text: "",
+        class: "",
+        type: "",
+        url: "/",
+    },
+],
 };
