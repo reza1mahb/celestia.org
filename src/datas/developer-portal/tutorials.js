423b5f3dbf36368e71bf7ddd069860c39a5fafcb
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-06-14 08:35:52 +0930

    Update tutorials.js

--- src/datas/developer-portal/tutorials.js
+++ src/datas/developer-portal/tutorials.js
@@ -24,5 +24,38 @@ export const tutorials = {
 				url: "https://docs.dymension.xyz/",
 			},
 		},
+		{
+			id: 3,
+			title: "OP Stack",
+			text: "The OP Stack is a modular, open-source blueprint for highly scalable, highly interoperable blockchains of all kinds.",
+			image: "developer-portal/opstack.png",
+			type: "external",
+			link: {
+				text:"Explore OP Stack",
+				url: "https://docs.celestia.org/category/optimism/",
+			},
+		},
+		{
+			id: 4,
+			title: "Sovereign SDK",
+			text: "A framework for building seamlessly scalable and interoperable rollups that can run on any blockchain.",
+			image: "developer-portal/sovereign.png",
+			type: "external",
+			link: {
+				text:"Explore Sovereign SDK",
+				url: "https://github.com/Sovereign-Labs/sovereign-sdk/tree/main/examples/demo-rollup",
+			},
+		},
+		{
+			id: 5,
+			title: "Caldera SDK",
+			text: "Caldera makes it easy to launch performant, customizable blockchain rollups.",
+			image: "developer-portal/caldera.png",
+			type: "external",
+			link: {
+				text:"Explore Caldera",
+				url: "https://calderaxyz.gitbook.io/caldera-documentation/getting-started/overview",
+			},
+		},
 	],
 };
