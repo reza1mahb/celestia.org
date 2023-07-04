f07f731088da99422a6b7755b65cd7489a748cc3
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-04 20:58:14 +0930

    Add files via upload

---
+++ src/pages/glossary/NMT.js
@@ -0,0 +1,64 @@
+import * as React from "react"
+
+import {FooterBoxes} from "../../datas/team/content";
+import Layout from "../../components/layout";
+import BreadCrumb from "../../components/breadcrumb";
+import SocialShare from "../../components/socialShare";
+import GlossaryNav from "../../components/glossary-nav";
+import {Helmet} from "react-helmet";
+import twitterCardImage from "../../images/glossary-twitter-card.png";
+
+const title = 'NMT';
+
+class GlossaryContent extends React.Component {
+    render() {
+        return (
+            <div className={'glossary-content'}>
+                <p>NMT stands for <a href="https://docs.celestia.org/concepts/how-celestia-works/data-availability-layer/#namespaced-merkle-trees-nmts">Namespaced Merkle Tree</a>.
+                </p>
+
+            </div>
+        )
+    }
+}
+
+class GlossarySubpage extends React.Component {
+    render() {
+        return (
+            <Layout footerBoxes={FooterBoxes}>
+                <Helmet>
+                    <title>Celestia - {title}</title>
+                    <meta property="og:url" content={this.props.location.href} />
+                    <meta property="og:title" content={'Celestia - '+title} />
+                    <meta property="og:description" content="" />
+                    <meta property="og:image" content={twitterCardImage} />
+
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:site" content="@CelestiaOrg" />
+                    <meta name="twitter:title" content={'Celestia - '+title} />
+                    <meta name="twitter:description" content="" />
+                    <meta name="twitter:image" content={twitterCardImage} />
+                </Helmet>
+                <div className={'glossary-subpage'}>
+                    <main>
+                        <div className={'container'}>
+                            <BreadCrumb title={title}/>
+                            <h1 className={'main mb-4'}>{title}</h1>
+
+                            <SocialShare title={title} url={this.props.location.href}/>
+
+                            <GlossaryContent/>
+
+                        </div>
+
+                        <div className={'container wide'}>
+                            <GlossaryNav url={this.props.location.state && this.props.location.state.url}/>
+                        </div>
+                    </main>
+                </div>
+            </Layout>
+        )
+    }
+}
+
+export default GlossarySubpage
