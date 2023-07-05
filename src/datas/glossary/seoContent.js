0cd98107a77d877eb39503eddfa8b973019b0341
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:33:09 +0930

    Add files via upload

---
+++ src/datas/glossary/seoContent.js
@@ -0,0 +1,5 @@
+export const seoContent = {
+	title: "glossary | celestia.org",
+	description: "Explore new terms about Celestia and modular blockchains.",
+    image: "celestia-default-og-image.jpg",
+};
