8607f8060a6657085aa7f74a7822b4faca698e54
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 16:57:20 +0930

    Update seoContent.js

--- src/datas/ecosystem/seoContent.js
+++ src/datas/ecosystem/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
-	title: "Celestia - Ecosystem",
-	description: "Celestia Ecosystem provides a wide range of apps and services built in the Celestia ecosystem. This page provides thematic focus to the diversity of services and apps developed on top of Celestia Tech.",
+	title: "Ecosystem | celestia.org",
+	description: "Celestia Ecosystem provides a wide range of apps and services built in the Celestia ecosystem.",
     image: "/ecosystem-page.jpg",
 };
