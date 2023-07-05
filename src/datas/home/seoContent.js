d6844270f5ac7e0895689c846e74a095a285155a
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:01:47 +0930

    Update seoContent.js

--- src/datas/home/seoContent.js
+++ src/datas/home/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
-	title: "Celestia - Home",
+	title: "Home | celestia.org",
 	description: "Celestia is a modular consensus and data network, built to enable anyone to easily deploy their own blockchain with minimal overhead.",
     image: "/celestia-default-og-image.jpg",
 };
