c9986048fa234cdb7f5628df027e00ea5e1b0ded
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:06:31 +0930

    Update seoContent.js

--- src/datas/press/seoContent.js
+++ src/datas/press/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
-	title: "Celestia - Press",
-	description: "Find Branding and Press Resources, Socilal Media channels, Enquiries and News about Celestia.",
-    image: "",
+	title: "Press | celestia.org",
+	description: "Find branding, news, social channels, and press contacts.",
+    image: "/celestia-default-og-image.jpg",
 };
