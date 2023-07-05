95390251abd0ea3a5d7e02e1961bc58819016fff
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:23:46 +0930

    Update seoContent.js

--- src/datas/developer-portal/seoContent.js
+++ src/datas/developer-portal/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "Developer Portal | celestia.org",
 	description: "The homepage for Celestia developers. Tutorials. Resources. Community.",
-    image: "Developer-Portal.jpg",
+    image: "developer-portal.jpg",
 };
