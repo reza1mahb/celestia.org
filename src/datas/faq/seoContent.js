af6e3c5eef9ed6ac341dcf37ed5092c57ee06cf1
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:02:38 +0930

    Update seoContent.js

--- src/datas/faq/seoContent.js
+++ src/datas/faq/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "FAQ | celestia.org",
 	description: "Find answers to frequently asked questions about Celestia.",
-    image: "",
+    image: "/celestia-default-og-image.jpg",
 };
