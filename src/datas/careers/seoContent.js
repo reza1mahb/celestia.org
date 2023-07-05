cd098db5a8bd5af7e7f0cad4567afb163319ded0
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:02:10 +0930

    Update seoContent.js

--- src/datas/careers/seoContent.js
+++ src/datas/careers/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "Careers | celestia.org",
 	description: "Join our team of leading engineers, researchers, and entrepreneurs in pioneering the first modular blockchain design.",
-    image: "",
+    image: "/celestia-default-og-image.jpg",
 };
