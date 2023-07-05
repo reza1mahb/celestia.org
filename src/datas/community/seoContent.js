903993ea4208c1042649930e56930e10cfd3f254
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:02:23 +0930

    Update seoContent.js

--- src/datas/community/seoContent.js
+++ src/datas/community/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "Community | celestia.org",
 	description: "Discover community hubs, discussion forums, and resources that are used by the global Celestia community.",
-    image: "",
+    image: "/celestia-default-og-image.jpg",
 };
