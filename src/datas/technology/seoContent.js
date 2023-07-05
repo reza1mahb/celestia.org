06b22ad9821e3e9f807df29c850a0e5e5ef45fa0
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-05 17:13:55 +0930

    Update seoContent.js

--- src/datas/technology/seoContent.js
+++ src/datas/technology/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
-	title: "Celestia - Technology",
-	description: "Celestia is pioneering a new paradigm in blockchain design. A minimal, modular consensus layer for rollups.",
-    image: "",
+	title: "Technology | celestia.org",
+	description: "Learn about the core technology powering Celestia's new modular blockchain architecture.",
+    image: "celestia-default-og-image.jpg",
 };
