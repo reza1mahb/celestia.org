4a948edce34b0a2310b2943322f8f8650c62b681
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-06 09:45:42 +0930

    Update seoContent.js

--- src/datas/resources/seoContent.js
+++ src/datas/resources/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "Resources | celestia.org",
 	description: "Explore blog posts, podcasts episodes, YouTube videos, and research papers about the Celestia network.",
-    image: "celestia-default-og-image.jpg",
+    image: "/celestia-default-og-image.jpg",
 };
