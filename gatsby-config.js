348322e9fc85b8210aa93bb9e86367fb025163ae
Tamás <t.gabor80312@gmail.com>
2023-07-06 11:16:47 +0200

    Update seoContent.js and  siteMeta url

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
     title: `The first modular blockchain network`,
     description: `Celestia is a modular consensus and data network, built to enable anyone to easily deploy their own blockchain with minimal overhead.`,
     author: `@CelestiaOrg`,
-    // siteUrl: `https://celestia.org`,
-    siteUrl: `https://dev.lazyledger.org`,
+    siteUrl: `https://celestia.org`,
+    // siteUrl: `https://dev.lazyledger.org`, <-- switch to this url for OG meta previews on https://dev.lazyledger.org
     image: `/celestia-default-og-image.jpg`,
   },
   
--- src/datas/team/seoContent.js
+++ src/datas/team/seoContent.js
@@ -1,5 +1,5 @@
 export const seoContent = {
 	title: "Team | celestia.org",
 	description: "Meet the team building the Celestia network.",
-    image: "celestia-default-og-image.jpg",
+    image: "/celestia-default-og-image.jpg",
 };
