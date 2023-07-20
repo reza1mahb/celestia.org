bbc1d4c1d5844a79dc25408e2efdfe399987a2b9
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-20 17:02:46 +0200

    Update banner.js

--- src/components/modules/banner.js
+++ src/components/modules/banner.js
@@ -32,11 +32,11 @@ export default class Banner extends React.Component {
                     {/* <div className={'col-2 position-relative d-none d-md-flex'}>
                         <Image alt={'Modular Summit'} filename={'modular-summit.svg'} />
                     </div> */}
-                    <div className={'col-auto'}>Modular Fellows applications are now open!</div>
+                    <div className={'col-auto'}>Modular Summit day 1 is live!</div>
 
                     <a className={'col-auto'} href="https://blog.celestia.org/introducing-modular-fellows" target={'_blank'} rel={'noreferrer'}>
                         <div>
-                            <button className={'button-simple sm'}>Learn more</button>
+                            <button className={'button-simple sm'}>Watch now</button>
                         </div>
                     </a>
                 </div>
