6cf06328e801f1eb4f661e22a972ca7b992b981b
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-07-20 08:04:26 +0200

    Update hero-data.js

--- src/datas/run-a-node/hero-data.js
+++ src/datas/run-a-node/hero-data.js
@@ -1,5 +1,5 @@
 export const heroData = {
-	title: "Run a Node",
+	title: "Run a light node",
 	text: "Enter an internet where users are first-class citizens. Own your assets while securing Celestia.",
 	buttons: [
 		{
@@ -7,12 +7,6 @@ export const heroData = {
 			class: "simple",
 			type: "anchor",
 			url: "start-up-a-node",
-		},
-		{
-			text: "Ask a question",
-			class: "white",
-			type: "external",
-			url: "https://discord.com/invite/YsnTPcSfWQ",
-		},
+		}
 	],
 };
