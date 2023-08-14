b36cc316d7409c2dd93f2b887709d6e6238523f5
Tamás <t.gabor80312@gmail.com>
2023-08-14 17:40:22 +0200

    updates

---
+++ src/datas/what-is-tia/wallets.js
@@ -0,0 +1,42 @@
+export const wallets = {
+	title: "Wallets",
+    details: "There are multiple wallet options that support TIA. Choose the wallet that fits your requirements.",
+	items: [
+		{
+			id: 1,
+			title: "Keplr Wallet",
+            device: ["Mobile", "Desktop", "Staking"],
+			image: "what-is-tia/kepler-icon.jpg",
+            backdropClass: "opacity-50",
+            url: "",
+			type: "external",
+		},
+		{
+			id: 2,
+			title: "Cosmostation",
+            device: ["Mobile", "Desktop", "Staking"],
+			image: "what-is-tia/cosmostation-icon.jpg",
+            backdropClass: "opacity-100",
+            url: "",
+			type: "external",
+		},
+		{
+			id: 3,
+			title: "Leap",
+			device: ["Mobile", "Desktop", "Staking"],
+			image: "what-is-tia/leap-icon.jpg",
+            backdropClass: "opacity-100",
+            url: "",
+			type: "external",
+		},
+		{
+			id: 4,
+			title: "Ledger",
+			device: ["Mobile", "Desktop", "Staking"],
+			image: "what-is-tia/ledger-icon.jpg",
+            backdropClass: "opacity-50",
+            url: "",
+			type: "external",
+		},
+	],
+};
--- src/pages/what-is-tia.js
+++ src/pages/what-is-tia.js
@@ -1,21 +1,19 @@
 import * as React from "react";
 
 import { heroData } from "../datas/what-is-tia/hero-data";
-import { getStarted } from "../datas/what-is-tia/get-started";
 import { role } from "../datas/what-is-tia/role";
-import { resources } from "../datas/what-is-tia/resources";
+import { wallets } from "../datas/what-is-tia/wallets";
 import { FooterBoxes } from "../datas/what-is-tia/content";
 
 import Layout from "../components/layout";
-import IconCard from "../components/modules/icon-card";
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 import { seoContent } from "../datas/run-a-node/seoContent";
 import SEO from "../components/seo";
 import Image from "../components/imageComponent";
 
-import image2 from "../images/what-is-tia/what-is-tia-custody-1.png";
-import image3 from "../images/what-is-tia/what-is-tia-custody-2.png";
+import image1 from "../images/what-is-tia/what-is-tia-custody-1.png";
+import image2 from "../images/what-is-tia/what-is-tia-custody-2.png";
 
 const DevPortal = () => {
 	return (
@@ -26,19 +24,19 @@ const DevPortal = () => {
 					<section className='hero'>
 						<div className={"container"}>
 							<h1 className={"main mb-3 mb-md-4"}>{heroData.title}</h1>
-							<div className={"row"}>
+							<div className={"main-text row"}>
 								<div className={"col-auto"}>
 									<div className={"subtitle"} dangerouslySetInnerHTML={{ __html: heroData.text }} />
 									<AnchorLink
 										to={`/run-a-light-node#${heroData.buttons[0].url}`}
-										className={"button button-" + heroData.buttons[0].class}
+										className={"mb-3 button button-" + heroData.buttons[0].class}
 										stripHash
 									>
 										{heroData.buttons[0].text}
 									</AnchorLink>
 									<a
 										href={`${heroData.buttons[1].url}`}
-										className={"button button-" + heroData.buttons[1].class}
+										className={"mb-3 button button-" + heroData.buttons[1].class}
 										target={"_blank"}
 										rel={"noreferrer"}
 									>
@@ -49,11 +47,11 @@ const DevPortal = () => {
 						</div>
 					</section>
 
-					<section className='role'>
+					<section className='tia-role'>
 						<div className={"container"}>
-							<div className={"row flex-wrap align-items-center justify-content-between my-2 pt-2 pt-lg-5 pb-3"}>
+							<div className={"row flex-wrap align-items-center justify-content-between"}>
 								<h2>{role.title}</h2>
-								<div className='role-items d-flex justify-content-between'>
+								<div className='role-items'>
 									{role.items.map(function (item) {
 										return (
 											<div className='role-card'>
@@ -73,8 +71,8 @@ const DevPortal = () => {
 					<section className={"custody-stacking"}>
 						<div className={"container"}>
 							<h2 className='custody-stacking-title'>Custody and staking</h2>
-							<div className={"row justify-content-between align-items-center"}>
-								<div className='col-12 col-lg-6'>
+							<div className={"custody-stacking-item row justify-content-between align-items-center"}>
+								<div className='col-12 col-lg-6 order-1 order-lg-0'>
 									<div className={"text-box"}>
 										<h2 className='title'>You own your TIA</h2>
 										<div className='custody-text-box'>
@@ -92,14 +90,14 @@ const DevPortal = () => {
 										</div>
 									</div>
 								</div>
-								<div className={"image-box--node col-auto"}>
+								<div className={"image-box--node col-auto order-0 order-lg-1"}>
 									<div className='image-wrapper'>
-										<img className='img' style={{ width: `100%`, maxWidth: `470px` }} src={image2} alt={""} />
+										<img className='img' style={{ width: `100%`, maxWidth: `470px` }} src={image1} alt={""} />
 									</div>
 								</div>
 							</div>
-							<div className={"row justify-content-between align-items-center"}>
-								<div className={"image-box--node col-12 col-lg-6"}>
+							<div className={"custody-stacking-item row justify-content-between align-items-lg-center"}>
+								<div className={"image-box--node col-auto"}>
 									<div className='image-wrapper'>
 										<img className='img' style={{ width: `100%`, maxWidth: `470px` }} src={image2} alt={""} />
 									</div>
@@ -120,7 +118,7 @@ const DevPortal = () => {
 											validator that will stake it on their behalf. TIA delegations can always be retrieved from validators
 											after undergoing a 3-week unbonding period.
 											<div className='link-wrapper'>
-												<a className='link' href="/">
+												<a className='link' href='/'>
 													View Documentation
 													<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none'>
 														<path
@@ -146,62 +144,107 @@ const DevPortal = () => {
 						</div>
 					</section>
 
-					<section className={"start-up"} id={`${heroData.buttons[0]?.url.replace(/\s+/g, "-").toLowerCase()}`}>
+					<section className='wallets'>
 						<div className={"container"}>
-							<div className={"row justify-content-between align-items-center"}>
-								<div className={"text-box col-12 col-lg-6"}>
-									<h2 className='with-decor'>Start up a light node</h2>
-									<p className='description'>
-										There are many ways to run a Celestia light node. Pick an option that fits you best to get your light node up
-										and running.
-									</p>
-								</div>
-								<div className={"image-box--startup col-12 col-lg-6"}>
-									<div className='image-wrapper'>
-										<img src={image3} alt={""} />
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-
-					<section className='get-started'>
-						<div className={"container"}>
-							{getStarted.description && <div className={"description"}>{getStarted.description}</div>}
-							<div className={"row gx-3 gy-3 pt-4 pb-3"}>
-								{getStarted.items.map(function (item) {
+							<h2 className='wallets-title'>{wallets.title}</h2>
+							<p>{wallets.details}</p>
+							<div className={"three-col-grid pt-4 pt-lg-5"}>
+								{wallets.items.map(function (item) {
 									return (
-										<IconCard
-											className='icon-card-wrapper col-12 col-md min-width-33'
-											key={item.id}
-											content={item}
-											variant={"anchor no-image"}
-										/>
+										<div className='wallet-card-wrapper'>
+											<div className={`wallet-blurred-image ${item.backdropClass}`}>
+												<Image alt={item.title} filename={item.image} />
+											</div>
+											<div className='wallet-card'>
+												<div className='wallet-container'>
+													<div className='wallet-image-wrapper'>
+														<Image alt={item.title} filename={item.image} />
+													</div>
+													<div>
+														<h3 className='wallet-card-title'>{item.title}</h3>
+														<div className='d-flex justify-content-start'>
+															{item.device.map(function (item) {
+																return (
+																	<div className='wallet-device-item d-flex flex-row align-items-center'>
+																		<div className='col-auto pe-2'>
+																			<svg
+																				width={12}
+																				height={9}
+																				viewBox='0 0 12 9'
+																				fill='none'
+																				xmlns='http://www.w3.org/2000/svg'
+																			>
+																				<path d='M1 4L4.5 7.5L11 1' stroke='#52C01E' strokeWidth='1.5' />
+																			</svg>
+																		</div>
+																		<div className='col'>
+																			<p className='wallet-card-text'>{item}</p>
+																		</div>
+																	</div>
+																);
+															})}
+														</div>
+													</div>
+												</div>
+												<div className='link-wrapper'>
+													<a className='link' href={item.url}>
+														Download
+														<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none'>
+															<path
+																stroke='#7B2BF9'
+																stroke-linecap='square'
+																stroke-width='1.5'
+																d='M3.61218 12.0721L11.0761 4.60823'
+															/>
+															<path
+																stroke='#7B2BF9'
+																stroke-linecap='square'
+																stroke-linejoin='bevel'
+																stroke-width='1.5'
+																d='M11.5254 11.0752V4.00413H4.45432'
+															/>
+														</svg>
+													</a>
+												</div>
+											</div>
+										</div>
 									);
 								})}
 							</div>
 						</div>
 					</section>
 
-					<section className='resources' id={`${getStarted.items[2].title.replace(/\s+/g, "-").toLowerCase()}`}>
+					<section className={"managing-tia"}>
 						<div className={"container"}>
-							<div className={"row flex-wrap align-items-center justify-content-between my-2 pt-2 pt-lg-5 pb-3"}>
-								<div className='col-12 col-xl-4 mb-5 mb-md-0'>
-									<h2 className={"with-decor"}>{resources.title}</h2>
-									{resources.description && <div className={"description"}>{resources.description}</div>}
+							<div className={"row justify-content-between align-items-center"}>
+								<div className='col-12 col-lg-6 text-box'>
+									<h2 className='title'>Managing TIA as a rollup developer</h2>
+									<p>
+										Rollup developers need to submit and pay for data in addition to managing their rollup’s TIA balance for
+										operation.
+									</p>
 								</div>
-								<div className='col-12 col-xl-8'>
-									<div className='gx-3 gy-5 row'>
-										{resources.items.map(function (item) {
-											return (
-												<IconCard
-													className='icon-card-wrapper col-12 col-md-4'
-													key={item.id}
-													content={item}
-													variant={"vertical"}
+								<div className={"col-12 col-lg-6 link-card"}>
+									<div className='link-title'>Create wallet with celestia-app</div>
+									<div className='link-wrapper'>
+										<a className='link' href='/'>
+											CTA
+											<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none'>
+												<path
+													stroke='#7B2BF9'
+													stroke-linecap='square'
+													stroke-width='1.5'
+													d='M3.61218 12.0721L11.0761 4.60823'
 												/>
-											);
-										})}
+												<path
+													stroke='#7B2BF9'
+													stroke-linecap='square'
+													stroke-linejoin='bevel'
+													stroke-width='1.5'
+													d='M11.5254 11.0752V4.00413H4.45432'
+												/>
+											</svg>
+										</a>
 									</div>
 								</div>
 							</div>
