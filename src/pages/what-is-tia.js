34c20483e89b481a5e90513631078ea5a044d36c
Tamás <t.gabor80312@gmail.com>
2023-08-15 12:09:25 +0200

    fix

--- src/pages/what-is-tia.js
+++ src/pages/what-is-tia.js
@@ -72,7 +72,7 @@ const DevPortal = () => {
 					<section className={"custody-stacking"}>
 						<div className={"container"}>
 							<h2 className='custody-stacking-title'>Custody and staking</h2>
-							<div className={"d-flex flex-column flex-lg-row custody-stacking-item justify-content-between align-items-center"}>
+							<div className={"d-flex flex-column flex-lg-row custody-stacking-item justify-content-between align-items-lg-center"}>
 								<div className='col-12 col-lg-6 order-1 order-lg-0'>
 									<div className={"text-box"}>
 										<h2 className='title'>You own your TIA</h2>
