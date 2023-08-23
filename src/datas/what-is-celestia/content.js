915af4ff919be1f615925362c6bb2455b7b940a4
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-08-23 18:23:29 +0300

    Update content.js

--- src/datas/what-is-celestia/content.js
+++ src/datas/what-is-celestia/content.js
@@ -1,19 +1,19 @@
 export const FooterBoxes2 = [
     {
-        title: 'Start using Celestia',
+        title: 'Build your app',
         text: 'Deploy your own chain or smart contract on Celestia’s public testnets',
         link: {
             text: 'Find your best option',
-            href: 'https://docs.celestia.org/',
+            href: '/developer-portal/',
             id: 'build',
         }
     },
     {
         title: 'Explore what you can do',
-        text: 'Go through Celestia’s world of apps, run your own node, learn about TIA, or even do all three.',
+        text: 'Go through Celestia’s world of apps, run your own node, learn about TIA, or even do all three!',
         link: {
             text: 'Show me',
-            href: '/careers',
+            href: '',
             id: 'explore',
         }
     }
