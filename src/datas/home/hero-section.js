964bf78ab5620c7c994eaddb987c517b0c4dcf48
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-09-07 20:03:02 +0300

    Update hero-section.js

--- src/datas/home/hero-section.js
+++ src/datas/home/hero-section.js
@@ -3,16 +3,16 @@ export const heroData = {
     text: 'Celestia is a modular consensus and data network, built to enable anyone to easily deploy their own blockchain with minimal overhead.',
     buttons:[
             {
-                text: 'Learn Celestia',
-                class: 'simple',
-                type: 'internal',
-                url: '/what-is-celestia/'
-            },
-            {
                 text: 'Build modular',
                 class: 'white',
                 type: 'internal',
                 url: '/developer-portal/'
+            },
+            {
+                text: 'Learn Celestia',
+                class: 'simple',
+                type: 'internal',
+                url: '/what-is-celestia/'
             }
         ]
 }
