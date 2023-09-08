22479f6c6b534c2e75dadb5ede2c706ca9e063e9
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-09-08 20:10:20 +0300

    Update explore-celestia.js

--- src/datas/home/explore-celestia.js
+++ src/datas/home/explore-celestia.js
@@ -3,16 +3,16 @@ export const exploreCelestia = [
         title: 'Use an app',
         text: "Explore Celestia's ecosystem of applications and services",
         image: 'home/explore-1.png',
-        link: "/ecosystem/",
+        link: "/ecosystem",
     },{
         title: 'What is TIA?',
         text: 'Learn about the token powering Celestia',
         image: 'home/explore-2.png',
-        link: "/what-is-tia/",
+        link: "/what-is-tia",
     },{
         title: 'Run a light node',
         text: 'Help contribute to Celestia’s decentralization in as little as 2 clicks',
         image: 'home/explore-3.png',
-        link: "/run-a-light-node/",
+        link: "/run-a-light-node",
     }
 ]
