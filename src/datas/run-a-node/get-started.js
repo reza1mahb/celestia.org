0cc19c858ed448bd89df87dd60985f9a444d3936
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-10-13 23:24:36 +0800

    Update get-started.js

--- src/datas/run-a-node/get-started.js
+++ src/datas/run-a-node/get-started.js
@@ -4,32 +4,32 @@ export const getStarted = {
 	items: [
 		{
 			id: 1,
-			title: "Run light node via your command line",
-			text: "Startup your light node with the command line. This option is best if you’re familiar with using the command line.",
+			title: "Beginner",
+			text: "Kickstart your light node in 2-clicks. Only for m1 and m2 macs.",
 			type: "external",
 			link: {
-				text:"Go now",
-				url: "https://docs.celestia.org/nodes/light-node/",
+				text:"Download app",
+				url: "https://www.quasarapp.xyz/",
 			},
 		},
 		{
 			id: 2,
-			title: "Run light node in a virtual environment",
-			text: "Startup your light node through the command line in a virtual environment. Though this option still uses the command line, it can be completed in under 3 minutes and also works on Windows devices.",
+			title: "Intermediate",
+			text: "Power up your light node with the command line and Docker, all in under 3 minutes.",
 			type: "external",
 			link: {
-				text:"Start up",
+				text:"Follow the guide",
 				url: "https://docs.celestia.org/nodes/docker-images/",
 			},
 		},
 		{
 			id: 3,
-			title: "Run light node through an application",
-			text: "Boot up your light node through a user-friendly application. This is the easiest option, but it only works for m1 and m2 macs.",
+			title: "Advanced",
+			text: "Spin up your light node through the command line.",
 			type: "external",
 			link: {
-				text:"Learn more",
-				url: "https://www.quasarapp.xyz/",
+				text:"Take me to the docs",
+				url: "https://docs.celestia.org/nodes/light-node/",
 			},
 		},
 	],
