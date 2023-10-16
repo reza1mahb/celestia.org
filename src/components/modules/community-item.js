75562b9f1ad4f8b7f4992c2d4e6093c69e3ec316
Tamás <t.gabor80312@gmail.com>
2023-10-16 15:22:26 +0200

    updated CommunityItem component

--- src/components/modules/community-item.js
+++ src/components/modules/community-item.js
@@ -6,37 +6,37 @@ export default function CommunityItem({ imageClass, content, className }) {
 	if (content.type === "internal") {
 		return (
 			<div className={className}>
-				<div className={`community-item`}>
-					<div className='link-wrapper'>
-						<Link className='link' to={`${content.url}`}>
+				<Link className='link' to={`${content.url}`}>
+					<div className={`community-item`}>
+						<div className='link-wrapper'>
 							<div className={`logo-container ${imageClass}`}>
 								<Image alt={content.title} filename={content.image} />
 							</div>
-						</Link>
+						</div>
+						<div className='text-box'>
+							<div className={"title"}>{content.title}</div>
+							{content.text && <div className={"text"}>{content.text}</div>}
+						</div>
 					</div>
-					<div className='text-box'>
-						<div className={"title"}>{content.title}</div>
-						{content.text && <div className={"text"}>{content.text}</div>}
-					</div>
-				</div>
+				</Link>
 			</div>
 		);
 	} else if (content.type === "external" || content.type === undefined) {
 		return (
 			<div className={className}>
-				<div className={`community-item`}>
-					<div className='link-wrapper'>
-						<a className='link' href={content.url} target={"_blank"} rel={"noreferrer"} aria-label={content.text}>
+				<a className='link' href={content.url} target={"_blank"} rel={"noreferrer"} aria-label={content.text}>
+					<div className={`community-item`}>
+						<div className='link-wrapper'>
 							<div className={`logo-container ${imageClass}`}>
 								<Image alt={content.title} filename={content.image} />
 							</div>
-						</a>
+						</div>
+						<div className='text-box'>
+							<div className={"title"}>{content.title}</div>
+							{content.text && <div className={"text"}>{content.text}</div>}
+						</div>
 					</div>
-					<div className='text-box'>
-						<div className={"title"}>{content.title}</div>
-						{content.text && <div className={"text"}>{content.text}</div>}
-					</div>
-				</div>
+				</a>
 			</div>
 		);
 	}
