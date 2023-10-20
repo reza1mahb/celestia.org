20399b5c2ddec89c33386cab4f4e8e2259769666
Gabor Sera <gabor@designatives.com>
2023-10-20 13:44:31 +0200

    fix: plausible

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import {useEffect} from "react";
 
 export default function Layout({ children, footerBoxes, footerBoxes2 }) {
+
+	useEffect(() => {
+		const elements = document.querySelectorAll("[class^=plausible-event-name]")
+
+		if(window.plausible) {
+			elements.forEach(function (element) {
+				element.addEventListener("click", function (e) {
+					window.plausible(element.classList[0].substring(element.classList[0].indexOf('=') + 1))
+				}, false);
+			})
+		}
+	}, []);
+
 	return (
 		<>
 			<Header />
