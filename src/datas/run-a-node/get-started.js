621e811dd5b2b224ba351a1e94a2434d992b7ef5
gabros20 <t.gabor80312@gmail.com>
2023-11-06 15:09:31 +0100

    removed beginner box

--- src/datas/run-a-node/get-started.js
+++ src/datas/run-a-node/get-started.js
@@ -2,23 +2,23 @@ export const getStarted = {
 	title: "",
 	description: "",
 	items: [
-		{
-			id: 1,
-			title: "Beginner",
-			text: "Kickstart your light node in 2-clicks. Only for m1 and m2 macs.",
-			type: "external",
-			link: {
-				text:"Download app",
-				url: "https://www.quasarapp.xyz/",
-			},
-		},
+		// {
+		// 	id: 1,
+		// 	title: "Beginner",
+		// 	text: "Kickstart your light node in 2-clicks. Only for m1 and m2 macs.",
+		// 	type: "external",
+		// 	link: {
+		// 		text: "Download app",
+		// 		url: "https://www.quasarapp.xyz/",
+		// 	},
+		// },
 		{
 			id: 2,
 			title: "Intermediate",
 			text: "Power up your light node with the command line and Docker, all in under 3 minutes.",
 			type: "external",
 			link: {
-				text:"Follow the guide",
+				text: "Follow the guide",
 				url: "https://docs.celestia.org/nodes/docker-images/",
 			},
 		},
@@ -28,7 +28,7 @@ export const getStarted = {
 			text: "Spin up your light node through the command line.",
 			type: "external",
 			link: {
-				text:"Take me to the docs",
+				text: "Take me to the docs",
 				url: "https://docs.celestia.org/nodes/light-node/",
 			},
 		},
--- src/pages/run-a-light-node.js
+++ src/pages/run-a-light-node.js
@@ -27,11 +27,7 @@ const DevPortal = () => {
 							<div className={"row"}>
 								<div className={"col-auto"}>
 									<div className={"text"} dangerouslySetInnerHTML={{ __html: heroData.text }} />
-									<AnchorLink
-										to={`/run-a-light-node#${heroData.buttons[0].url}`}
-										className={"button button-" + heroData.buttons[0].class}
-										stripHash
-									>
+									<AnchorLink to={`/run-a-light-node#${heroData.buttons[0].url}`} className={"button button-" + heroData.buttons[0].class} stripHash>
 										{heroData.buttons[0].text}
 									</AnchorLink>
 									{/* <a
@@ -58,15 +54,13 @@ const DevPortal = () => {
 								<div className={"text-box col-12 col-lg-6"}>
 									<h2 className='title'>What is a node?</h2>
 									<p>
-										A node is a piece of software that runs on a device like a computer or a phone. Generally, a node verifies
-										that a blockchain works correctly and follows the rules. “Verifying” a blockchain can involve tasks like
-										checking transactions are correct.
+										A node is a piece of software that runs on a device like a computer or a phone. Generally, a node verifies that a blockchain works
+										correctly and follows the rules. “Verifying” a blockchain can involve tasks like checking transactions are correct.
 									</p>
 									<h2 className='title'>What is a Celestia light node?</h2>
 									<p>
-										Light nodes sound like you might expect. They are nodes that can run on cheaper hardware and slower internet
-										connections than other node types. The lower requirements make it more accessible for anyone to run a node
-										that verifies the chain.
+										Light nodes sound like you might expect. They are nodes that can run on cheaper hardware and slower internet connections than
+										other node types. The lower requirements make it more accessible for anyone to run a node that verifies the chain.
 									</p>
 								</div>
 							</div>
@@ -78,8 +72,7 @@ const DevPortal = () => {
 							<div className={"row justify-content-center align-items-center"}>
 								<h2 className='with-decor'>Start up a light node</h2>
 								<p className='description'>
-									There are many ways to run a Celestia light node. Pick an option that fits you best to get your light node up and
-									running.
+									There are many ways to run a Celestia light node. Pick an option that fits you best to get your light node up and running.
 								</p>
 							</div>
 						</div>
@@ -91,19 +84,14 @@ const DevPortal = () => {
 							<div className={"row gx-3 gy-3 pt-4 pb-3"}>
 								{getStarted.items.map(function (item) {
 									return (
-										<IconCard
-											className='icon-card-wrapper col-12 col-md min-width-33'
-											key={item.id}
-											content={item}
-											variant={"anchor no-image"}
-										/>
+										<IconCard className='icon-card-wrapper col-12 col-md min-width-33' key={item.id} content={item} variant={"anchor no-image"} />
 									);
 								})}
 							</div>
 						</div>
 					</section>
 
-					<section className='resources' id={`${getStarted.items[2].title.replace(/\s+/g, "-").toLowerCase()}`}>
+					<section className='resources' id={`${getStarted.items[1].title.replace(/\s+/g, "-").toLowerCase()}`}>
 						<div className={"container"}>
 							<div className={"row flex-wrap align-items-center justify-content-between my-2 pt-2 pt-lg-5 pb-3"}>
 								<div className='col-12 col-xl-4 mb-5 mb-md-0'>
@@ -113,14 +101,7 @@ const DevPortal = () => {
 								<div className='col-12 col-xl-8'>
 									<div className='gx-3 gy-5 row'>
 										{resources.items.map(function (item) {
-											return (
-												<IconCard
-													className='icon-card-wrapper col-12 col-md-4'
-													key={item.id}
-													content={item}
-													variant={"vertical"}
-												/>
-											);
+											return <IconCard className='icon-card-wrapper col-12 col-md-4' key={item.id} content={item} variant={"vertical"} />;
 										})}
 									</div>
 								</div>
