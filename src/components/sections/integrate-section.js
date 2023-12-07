75e859d287fd11668130257e8528ff694e24ce34
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-12-08 00:43:40 +1030

    Update integrate-section.js

--- src/components/sections/integrate-section.js
+++ src/components/sections/integrate-section.js
@@ -12,7 +12,7 @@ const IntegrateSection = () => {
                             <div className={'text'}>
                                 Use Celestia as the DA layer for your Ethereum L2.
                             </div>
-                            <a href={'/'} className={'button button-simple'}>View Blobstream</a>
+                            <a href={'/'} className={'button button-simple'}>Blobstream documentation</a>
                         </div>
                         <div className={'col box ms-md-1'}>
                             <div className={'subtitle'}>Integrate</div>
@@ -20,7 +20,7 @@ const IntegrateSection = () => {
                             <div className={'text'}>
                                 Use the celestia-node API to publish and retrieve transactions from Celestia.
                             </div>
-                            <a href={'/'} className={'button button-simple'}>View Documentatio</a>
+                            <a href={'/'} className={'button button-simple'}>Node API Documentatio</a>
                         </div>
                     </div>
                 </div>
