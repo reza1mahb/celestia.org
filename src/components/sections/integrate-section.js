6dae4dd77a9b6021228a234758b8210f30f6549a
Gábor Tamás <t.gabor880312@gmail.com>
2023-12-08 16:40:50 +0000

    removed "integrate" text

--- src/components/sections/integrate-section.js
+++ src/components/sections/integrate-section.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const IntegrateSection = ({content, anchorId}) => {
-    return (
-        <section className={'integrate-section pb-0'} id={`${content.items[anchorId].title.replace(/\s+/g, "-").toLowerCase()}`}>
-            <div className={'container'}>
-                <div className={'row'}>
-                    <div className={'row'}>
-                        <div className={'col box me-md-1'}>
-                            <div className={'subtitle'}>Integrate</div>
-                            <div className={'title'}>Blobstream</div>
-                            <div className={'text'}>
-                                Use Celestia as the DA layer for your Ethereum L2.
-                            </div>
-                            <a href={'https://docs.celestia.org/developers/blobstream/'} className={'button button-simple'}>Blobstream documentation</a>
-                        </div>
-                        <div className={'col box ms-md-1'}>
-                            <div className={'subtitle'}>Integrate</div>
-                            <div className={'title'}>Node API</div>
-                            <div className={'text'}>
-                                Use the celestia-node API to publish and retrieve transactions from Celestia.
-                            </div>
-                            <a href={'https://node-rpc-docs.celestia.org/?version=v0.12.0/'} className={'button button-simple'}>Node API Documentatio</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+const IntegrateSection = ({ content, anchorId }) => {
+	return (
+		<section className={"integrate-section pb-0"} id={`${content.items[anchorId].title.replace(/\s+/g, "-").toLowerCase()}`}>
+			<div className={"container"}>
+				<div className={"row"}>
+					<div className={"row"}>
+						<div className={"col box me-md-1"}>
+							<div className={"title"}>Blobstream</div>
+							<div className={"text"}>Use Celestia as the DA layer for your Ethereum L2.</div>
+							<a href={"https://docs.celestia.org/developers/blobstream/"} className={"button button-simple"}>
+								Blobstream documentation
+							</a>
+						</div>
+						<div className={"col box ms-md-1"}>
+							<div className={"title"}>Node API</div>
+							<div className={"text"}>Use the celestia-node API to publish and retrieve transactions from Celestia.</div>
+							<a href={"https://node-rpc-docs.celestia.org/?version=v0.12.0/"} className={"button button-simple"}>
+								Node API Documentatio
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default IntegrateSection;
