74937d0b1bd272ae5aaa3a9abc189b861de682c5
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2023-12-20 00:37:09 +1030

    Update frameworks.js

--- src/datas/developer-portal/frameworks.js
+++ src/datas/developer-portal/frameworks.js
@@ -10,7 +10,9 @@ export const frameworks = {
 			text: "Arbitrum Orbit is the ideal way to permissionlessly launch your own custom chain.",
 			image: "developer-portal/arbitrum.png",
 			link: {
-				text: "Coming Soon",
+				text: "Explore Arbitrum Orbit",
+				url: "https://docs.celestia.org/developers/arbitrum-integration",
+				type: "external",
 			},
 		},
 		{
