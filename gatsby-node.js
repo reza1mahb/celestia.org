ca9c6cd7e35fec6da8e64eba1703e8a6201cc4bd
Gábor Tamás <t.gabor880312@gmail.com>
2024-01-03 15:45:17 +0100

    update: testing redirect for /deploy to /build/#deploy

---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+exports.createPages = async ({ graphql, actions }) => {
+	const { createRedirect } = actions;
+
+	createRedirect({
+		fromPath: `/deploy`,
+		toPath: `/build/#deploy`,
+		isPermanent: true,
+	});
+};
