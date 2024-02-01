a8de057fffc562c27f27d3f529bf2886486521e8
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-02-01 14:14:39 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ const IndexPage = () => {
 							direction={"ltr"}
 							title={"Access abundance"}
 							text={`
-								<p>Tap into the abundant throughput enabled by <a href='https://celestia.org/what-is-celestia/#what-is-data-availability-sampling' target='_blank' rel='noopener noreferrer'>Data Availability Sampling (DAS)</a>, the first architecture that securely scales with the number of users.</p>
+								<p>Tap into the abundant throughput enabled by <a href='https://celestia.org/what-is-celestia/#what-is-data-availability-sampling' target='_blank' rel='noopener noreferrer'>data availability sampling (DAS)</a>, the first architecture that securely scales with the number of users.</p>
 								<p>Anyone can directly verify and contribute to Celestia by <a href='https://celestia.org/run-a-light-node/' target='_blank' rel='noopener noreferrer'>running a light node</a>.</p>
 							`}
 							image={"graph-scale.png"}
